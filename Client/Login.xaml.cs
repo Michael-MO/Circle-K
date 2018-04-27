cc066cf782c00acb1fed9781758271a879afbf3a
Mikkel Krøyer <MikkelKroyer@hotmail.com>
2018-04-28 01:16:15 +0200

    V3.2 nu med login view.

--- Client/Login.xaml.cs
+++ Client/Login.xaml.cs
@@ -13,28 +13,18 @@ using Windows.UI.Xaml.Input;
 using Windows.UI.Xaml.Media;
 using Windows.UI.Xaml.Navigation;
 
-// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=234238
 
 namespace Client
 {
     /// <summary>
     /// An empty page that can be used on its own or navigated to within a Frame.
     /// </summary>
-    public sealed partial class MainPage : Page
+    public sealed partial class Login : Page
     {
-        public MainPage()
+        public Login()
         {
             this.InitializeComponent();
-        }
-
-        private void MainMenu(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(MainMenu), null);
-        }
-
-        private void Login(object sender, RoutedEventArgs e)
-        {
-            this.Frame.Navigate(typeof(Login), null);
         }
     }
 }

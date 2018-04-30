ad4498587290ec2bf6c378991faf1df199de0dd5
NiklasBidstrup <31881127+NiklasBidstrup@users.noreply.github.com>
2018-04-30 11:13:29 +0200

    Filled domain logic for domain classes

--- Client/Model/Domain/Station.cs
+++ Client/Model/Domain/Station.cs
@@ -3,10 +3,43 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Windows.Globalization.PhoneNumberFormatting;
 
 namespace Client.Model.Domain
 {
-    class Station
+    public class Station
     {
+        private string _phoneNumber;
+        private string _stationNumber;
+        private string _address;
+
+        public Station(string phoneNumber, string stationNumber, string address)
+        {
+            _phoneNumber = phoneNumber;
+            _stationNumber = stationNumber;
+            _address = address;
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+        }
+
+        public string StationNumber
+        {
+            get { return _stationNumber; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+        }
+
+
+
+
+
+
+
     }
 }

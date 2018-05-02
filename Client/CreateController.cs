f22f38d9b4b5bd1df072bdc4c5d20493a35d77c9
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-05-02 14:07:42 +0200

    Revert "Remove Commands"
    This reverts commit f0f9b6cd212ed60e71fc1fc6e76ab270b6cfc12e.

---
+++ Client/CreateController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels.Controller
+{
+    class CreateController : Employee
+    {
+
+        private override bool CanExecute()
+        {
+        }
+
+        private override void Execute()
+        {
+
+        }
+
+
+
+
+    }
+}
---
+++ Client/UpdateController.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.ViewModels.Controller
+{
+    class UpdateController : Employee
+    {
+
+        private override bool CanExecute()
+        {
+        }
+
+        private override void Execute()
+        {
+
+        }
+    }
+}

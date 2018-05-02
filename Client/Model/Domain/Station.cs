31a4a701647d533af56c99de70fac242155787c2
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-05-02 14:12:01 +0200

    Revert "Revert "Merge pull request #1 from Michael-MO/Domain-classes""
    This reverts commit 887279e23c6b6b95a02016efddbf22a72d4c7699.

---
+++ Client/Model/Domain/Station.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Windows.Globalization.PhoneNumberFormatting;
+
+namespace Client.Model.Domain
+{
+    public class Station
+    {
+        private string _phoneNumber;
+        private string _stationNumber;
+        private string _address;
+
+        public Station(string phoneNumber, string stationNumber, string address)
+        {
+            _phoneNumber = phoneNumber;
+            _stationNumber = stationNumber;
+            _address = address;
+        }
+
+        public string PhoneNumber
+        {
+            get { return _phoneNumber; }
+        }
+
+        public string StationNumber
+        {
+            get { return _stationNumber; }
+        }
+
+        public string Address
+        {
+            get { return _address; }
+        }
+
+
+
+
+
+
+
+    }
+}
---
+++ Client/Model/Domain/User.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model.Domain
+{
+    public abstract class User
+    {
+        protected string _accessLevel;
+        protected string _username;
+        protected string _password;
+
+
+
+        protected User(string accessLevel, string username, string password)
+        {
+            _accessLevel = accessLevel;
+            _username = username;
+            _password = password;
+        }
+
+        public string AccessLevel
+        {
+            get { return _accessLevel; }
+        }
+
+        public string Username
+        {
+            get { return _username; }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+        }
+
+
+        // Method for setting accesslevel
+            // Minimum Butikschef eller højere
+            // Kan kun give adgangsniveau lavere end ens eget niveau
+            // Gøres ved at man ændre "title" i employee classen, som så ændre accessLevel her
+            // Se Daniel billede på telefon
+
+
+        protected void ChangeAccesslevel(Employee employee, string accessLevel)
+        {
+
+        }
+
+
+        // Method for setting username
+
+
+
+        // Method for setting password
+
+
+    }
+}

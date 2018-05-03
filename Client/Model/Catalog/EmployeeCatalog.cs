b0ac4c31a7c35f87c00f48b7c4208001a1326b09
NiklasBidstrup <31881127+NiklasBidstrup@users.noreply.github.com>
2018-05-03 10:09:18 +0200

    1.0 - EmployeeCatalog - added class

---
+++ Client/Model/Catalog/EmployeeCatalog.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Client.Model.Catalog
+{
+    public class EmployeeCatalog : CatalogAppBase<Model.Domain.Employee, EmployeeViewData, EmployeePersistentData>
+    {
+        public EmployeeCatalog() : base("Employees")
+        {
+        }
+
+        public override Model.Domain.Employee CreateDomainObjectFromViewDataObject(EmployeeViewData vdObj)
+        {
+            return new Model.Domain.Employee
+            (
+                //Implementation 
+
+
+
+
+
+
+            );
+        }
+
+
+        public override EmployeeViewData CreateViewDataObject(Model.Domain.Employee obj)
+        {
+            return new EmployeeViewData
+            {
+                //Implementation
+
+
+
+
+
+            }
+        }
+
+        public override EmployeePersistentData CreatePersistentDataObject(Model.Domain.Employee obj)
+        {
+            return new EmployeePersistentData
+            {
+                //Implementation
+
+
+
+
+            }
+        }
+
+        public override Model.Domain.Employee CreateDomainObjectFromFromPersistentDataObject(
+            EmployeePersistentData pdObj)
+        {
+            return new Model.Domain.Employee
+            (
+                //Implementation
+
+
+
+            );
+        }
+
+
+
+
+
+
+    }
+}

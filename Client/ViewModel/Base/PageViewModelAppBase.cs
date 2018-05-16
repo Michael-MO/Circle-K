627495e4d436192f6989eab2b58924d1636eb449
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-05-16 11:01:48 +0200

    Added static itemSelected instancefields

--- Client/ViewModel/Base/PageViewModelAppBase.cs
+++ Client/ViewModel/Base/PageViewModelAppBase.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Windows.UI.Text;
 using Windows.UI.Xaml;
 using Data.InMemory.Interfaces;
+using Data.Transformed.Interfaces;
 using Extensions.Commands.Interfaces;
 using Extensions.ViewModel.Implementation;
 using Model.Interfaces;
@@ -16,6 +17,7 @@ namespace Client.ViewModel.Base
         where TViewData : class, ICopyable, IStorable, new()
     {
         private Dictionary<string, FontWeight> _menuFontWeights;
+        public static IDataWrapper<TViewData> ItemSelectedInstance;
 
         protected PageViewModelAppBase(ICatalog<TViewData> catalog, List<string> immutablecontrols,
             List<string> mutablecontrols)
@@ -45,6 +47,17 @@ namespace Client.ViewModel.Base
             _menuFontWeights.Add("ReadMenuItem", ViewStateService.ViewState == CRUDStates.ReadState ? FontWeights.Bold : FontWeights.Normal);
             _menuFontWeights.Add("UpdateMenuItem", ViewStateService.ViewState == CRUDStates.UpdateState ? FontWeights.Bold : FontWeights.Normal);
             _menuFontWeights.Add("DeleteMenuItem", ViewStateService.ViewState == CRUDStates.DeleteState ? FontWeights.Bold : FontWeights.Normal);
+        }
+
+        public override IDataWrapper<TViewData> ItemSelected
+        {
+            get { return base.ItemSelected; }
+
+            set
+            {
+                base.ItemSelected = value;
+                ItemSelectedInstance = base.ItemSelected;
+            }
         }
     }
 }

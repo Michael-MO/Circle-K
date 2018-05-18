772330accb45f1226caef20106ae3e812f32a52b
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-05-18 10:25:40 +0200

    Added DateTime DeletionDate propperty & String TerminationReason propperty

--- Client/Model/Catalog/EmployeeCatalog.cs
+++ Client/Model/Catalog/EmployeeCatalog.cs
@@ -29,6 +29,8 @@ namespace Client.Model.Catalog
                 vdObj.Mail,
                 vdObj.Station,
                 vdObj.IsActive,
+                vdObj.DeletionDate,
+                vdObj.TerminationReason,
                 vdObj.Username,
                 vdObj.Password,
                 vdObj.AccessLevel,
@@ -45,7 +47,8 @@ namespace Client.Model.Catalog
                 Title = obj.Title.TrimEnd(' '),
                 Name = obj.Name.TrimEnd(' '),
                 Address = obj.Address.TrimEnd(' '),
-                Mail = obj.Mail.TrimEnd(' ')
+                Mail = obj.Mail.TrimEnd(' '),
+                TerminationReason = obj.TerminationReason.TrimEnd(' ')
             };
         }
 
@@ -59,7 +62,8 @@ namespace Client.Model.Catalog
                 Name = obj.Name,
                 Address = obj.Address,
                 Mail = obj.Mail,
-                Station = obj.Station
+                Station = obj.Station,
+                TerminationReason = obj.TerminationReason   
                 //NOTICE! -  possibly add rest of the propperties
             };
         }
@@ -77,6 +81,8 @@ namespace Client.Model.Catalog
                 pdObj.Mail,
                 pdObj.Station,
                 pdObj.IsActive,
+                pdObj.DeletionDate,
+                pdObj.TerminationReason,
                 pdObj.Username,
                 pdObj.Password,
                 pdObj.AccessLevel,

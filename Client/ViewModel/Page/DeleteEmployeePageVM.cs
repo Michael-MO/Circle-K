d5c91e36361825b9fc893f168d9405751822ea29
DanielEASJ <31865128+DanielEASJ@users.noreply.github.com>
2018-05-18 11:23:18 +0200

    Finished Delete controller (need merge with master for missing propperties)

--- Client/ViewModel/Page/DeleteEmployeePageVM.cs
+++ Client/ViewModel/Page/DeleteEmployeePageVM.cs
@@ -6,15 +6,31 @@ using System.Threading.Tasks;
 using Client.DataTransformations.ViewData;
 using Client.Model.App;
 using Client.Model.Catalog;
+using Client.ViewModel.Controller;
 using Commands.Implementation;
+using Data.Transformed.Interfaces;
 using Extensions.Commands.Interfaces;
 
 namespace Client.ViewModel.Page
 {
     class DeleteEmployeePageVM : EmployeePageVM
     {
-        public DeleteCommandBase<EmployeeViewData> _deleteCommand 
-            = new DeleteCommandBase<EmployeeViewData>(ItemSelectedInstance, DomainModel.Instance.Employees, CanDoDelete );
+        private DeleteEmployeeController _deleteEmployeeController;
+        private IDataWrapper<EmployeeViewData> _deleteInstance;
+        public DeleteCommandBase<EmployeeViewData> _deleteCommand
+            = new DeleteCommandBase<EmployeeViewData>(ItemSelectedInstance, DomainModel.Instance.Employees, CanDoDelete);
+
+        public DeleteEmployeePageVM()
+        {
+            _deleteInstance = ItemSelectedInstance;
+            ItemSelectedInstance = null;
+            _deleteEmployeeController = new DeleteEmployeeController(_deleteInstance, DomainModel.Instance.Employees, CanDoDelete);
+        }
+
+        public IDataWrapper<EmployeeViewData> DeleteInstance
+        {
+            get { return _deleteInstance; }
+        }
 
         protected override void SetupInitialViewState()
         {

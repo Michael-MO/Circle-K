d584b356827e760d8cf2561a0eb6ab6d218dc9d8
Michael-MO <Michael-MO@users.noreply.github.com>
2018-05-22 12:15:03 +0200

    Removed Random Whitespaces, Fix Indent & Update SQL-file.

--- WebService/Models/Employee.cs
+++ WebService/Models/Employee.cs
@@ -40,7 +40,6 @@ namespace WebService.Models
         [Column(TypeName = "Date")]
         public DateTime DeletionDate { get; set; }
 
-        [StringLength(500)]
         public string TerminationReason { get; set; }
 
         public int UserName { get; set; }
